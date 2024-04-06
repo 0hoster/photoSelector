9c0526889308b75b744b19b4235da0722813e73f
hoster <zh721548595@outlook.com>
2024-04-06 08:08:16 +0800

    deleted CMakeLists.txt.user, that shouldn't be in git

--- mainwindow.h
+++ mainwindow.h
@@ -2,14 +2,52 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QLabel>
+#include <QPushButton>
+#include <QFileInfo>
+#include <QFileInfoList>
+#include <QDir>
+#include <QPaintEvent>
+#include <QStatusBar>
+#include <QPainter>
+#include <QPixmap>
+#include <QImageReader>
+#include <QSize>
+#include <QKeyEvent>
 
 class MainWindow : public QMainWindow {
 Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = nullptr);
+private:
+    QLabel *statusLabel;
+    QDir root;
+    QFileInfoList images;
+    QFileInfoList::iterator current;
 
-    ~MainWindow();
+    void initUI();
+
+    void initSlots();
+
+    void setSuitableScreenSize();
+
+    void updateInfo();
+
+    QPixmap loadPixmap(const QString &filename);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+    void keyPressEvent(QKeyEvent *event) override;
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr, const QString &rootPath = "");
+
+    ~MainWindow() override;
+
+signals:
+
+    void currentImageChange();
 };
 
 #endif // MAINWINDOW_H
--- welcome.h
+++ welcome.h
@@ -10,6 +10,10 @@
 #include <QString>
 #include <QKeyEvent>
 #include <QMouseEvent>
+#include <QFileDialog>
+#include <QDir>
+#include <QFont>
+#include <QPalette>
 
 class Welcome : public QDialog {
 Q_OBJECT
@@ -18,19 +22,22 @@ private:
     QString rootFolder;
     QLabel *imageLabel;
     QLabel *welcomeLabel;
+    QLabel *tipLabel;
 
     void initUI();
 
     void initSlots();
 
+    void chooseRootFolder();
+
 protected:
     void keyPressEvent(QKeyEvent *event) override;
-    void mouseDoubleClickEvent(QMouseEvent *event)override;
+    void mousePressEvent(QMouseEvent *event)override;
 
 public:
     explicit Welcome(QWidget *parent = nullptr);
-
     ~Welcome() override;
+    QString getRootDir();
 };
 
 #endif // WELCOME_H
